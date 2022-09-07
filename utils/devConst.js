c699908be529b18e2a4a3284881e985d1c6958e8
Maxiair <maxiair@yandex.ru>
2022-09-07 17:58:33 +0300

    feat: correct devconst

--- utils/devConst.js
+++ utils/devConst.js
@@ -1,10 +1,10 @@
 const {
-  NODE_ENV, JWT_SECRET, MONGO_PATH, MONGO_PORT,
+  NODE_ENV, JWT_SECRET, MONGO_PATH, EXPRESS_PORT,
 } = process.env;
 
 module.exports.SECRET_KEY = NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key';
 module.exports.DB_PATH = NODE_ENV === 'production' ? MONGO_PATH : 'mongodb://127.0.0.1:27017/moviesdb';
-module.exports.PORT = NODE_ENV === 'production' ? MONGO_PORT : 3000;
+module.exports.PORT = NODE_ENV === 'production' ? EXPRESS_PORT : 3000;
 module.exports.EXPIRE_TOKEN = '7d';
 module.exports.saltRounds = 10;
 module.exports.corsOptions = {
